b5d4e8bca2e10a10c1767b062a3e8378143aa041
Succo Rolando <rolandosucco@gmail.com>
2021-02-09 09:21:08 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,36 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Main extends JFrame{
+	Main(){
+this.setSize(500, 500);
+this.setVisible(true);
+		}
+  public static void main(String[] args) {
+	
+    // creating instance of JFrame
+   // JFrame f = new JFrame("Ciao io sono una finestra");
+ new Main();
+    /* creating instance of JButton
+    //JButton b = new JButton("Click me!");
+
+    // x axis, y axis, width, height
+   // b.setBounds(80, 100, 250, 40);
+
+    // add event listener
+    //b.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        System.out.println("Hello world!");
+      }
+    });
+
+    // add button to JFrame
+    f.add(b);*/
+
+   
+    //f.setLayout(null);
+    // make the frame visible
+   //setVisible(true);
+  }
+}
